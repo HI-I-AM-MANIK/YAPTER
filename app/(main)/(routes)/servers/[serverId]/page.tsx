11fee1349fe59649e27350f9478abdef0279fdf1
Manik Gupta <142866997+HI-I-AM-MANIK@users.noreply.github.com>
2026-07-05 03:23:35 +0530

    ServerPage done

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,55 @@
-const ServerPage = () => {
-  return <div>SERVER PAGE WORKING</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ServerPageProps {
+  params: Promise<{
+    serverId: string;
+  }>;
+}
+
+const ServerPage = async ({ params }: ServerPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return <RedirectToSignIn/>;
+  }
+
+  const { serverId } = await params;
+
+  const server = await db.server.findFirst({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const initialChannel = server.channels[0];
+
+  if (!initialChannel) {
+    return redirect("/");
+  }
+
+  return redirect(`/servers/${serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerPage;
